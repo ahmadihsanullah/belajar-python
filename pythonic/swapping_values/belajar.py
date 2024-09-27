d682cdd244d3659e0a4c648e67da722cfd8d96a9
ahmadihsanullah <ahma21327ti@student.nurulfikri.ac.id>
2024-09-27 08:36:10 +0700

    menukar nilai

---
+++ pythonic/swapping_values/belajar.py
@@ -0,0 +1,8 @@
+a, b, c, d, e = 10, 20, 30, 40, 50
+print('sebelum pertukaran')
+print(f'a={a}, b={b}, c={c}, d={d}, e={e}')
+print('------')
+a, b, c, d, e  = e, d, c, b, a
+print('setelah pertukaran')
+print(f'a={a}, b={b}, c={c}, d={d}, e={e}')
+
